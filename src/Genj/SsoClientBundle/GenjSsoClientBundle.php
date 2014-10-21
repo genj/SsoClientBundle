2bb8385ae515ffcf33c679aac36b8d7657e11037
Nico Kaag <nico.kaag@genj.nl>
2014-10-21 16:06:21 +0200

    Added basic bundle structure

---
+++ src/Genj/SsoClientBundle/GenjSsoClientBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Genj\SsoClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class GenjSsoClientBundle
+ *
+ * @package Genj\GenjSsoClientBundle
+ */
+class GenjSsoClientBundle extends Bundle
+{
+}
